318689e14aed485aea03ab018572f1bbef6ac8f2
carlobeltrame <carlo@beltra.me>
2025-09-11 12:09:20 +0200

    Correct spelling of BdP

--- app/controllers/pfadi_de/people_controller.rb
+++ app/controllers/pfadi_de/people_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-#  Copyright (c) 2025, BDP and DPSG. This file is part of
+#  Copyright (c) 2025, BdP and DPSG. This file is part of
 #  hitobito_pfadi_de and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito_pfadi_de.
