186be5d50b1c86312f8fa9d7cd0e12a674abd770
carlobeltrame <carlo@beltra.me>
2025-09-18 13:16:20 +0200

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_09_09_120000) do
+ActiveRecord::Schema[7.1].define(version: 2025_09_16_120000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -974,6 +974,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_09_09_120000) do
     t.string "iban"
     t.string "bic"
     t.string "bank_name"
+    t.string "payment_method", default: "invoice", null: false
     t.index ["authentication_token"], name: "index_people_on_authentication_token"
     t.index ["confirmation_token"], name: "index_people_on_confirmation_token", unique: true
     t.index ["email"], name: "index_people_on_email", unique: true
