fc533b671a2c7e321a15d1ad9f972a680c2a4a41
Matthias Viehweger <kronn@kronn.de>
2026-01-28 18:35:04 +0100

    Generate model for FeeRate

---
+++ app/models/fee_rate.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2026-2026, BdP and DPSG. This file is part of
+#  hitobito_pfadi_de and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_pfadi_de.
+
+class FeeRate < ApplicationRecord
+  belongs_to :fee_kind
+end
---
+++ spec/models/fee_rate_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2026-2026, BdP and DPSG. This file is part of
+#  hitobito_pfadi_de and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_pfadi_de.
+
+require "spec_helper"
+
+describe FeeRate, type: :model do
+  subject { fee_rates(:jahresbeitragssatz) }
+
+  describe "has validations, which enforce" do
+    it "valid fixtures" do
+      is_expected.to be_valid
+    end
+  end
+end
