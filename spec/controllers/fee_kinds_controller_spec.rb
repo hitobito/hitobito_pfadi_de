b5aa1a216121ce49492dfd79af5a57ee7a97c4ff
Matthias Viehweger <kronn@kronn.de>
2026-02-17 16:47:01 +0100

    Adapt specs to updated fixtures and rules

--- spec/controllers/fee_kinds_controller_spec.rb
+++ spec/controllers/fee_kinds_controller_spec.rb
@@ -6,15 +6,21 @@
 require "spec_helper"
 
 describe FeeKindsController do
-  let(:group) { Fabricate(Group::Bundesgeschaeftsstelle.sti_name, parent: groups(:root)) }
   let(:person) { people(:admin) }
-  let(:fee_kind) {
-    FeeKind.create!(layer: group, name: "Foo", parent: fee_kinds(:baden_wuerttemberg_kind))
-  }
+
+  let(:group) do
+    Fabricate(Group::Stamm.sti_name, name: "Falken", parent: groups(:baden_wuerttemberg))
+  end
+
+  let(:fee_kind) do
+    Fabricate(:fee_kind, layer: group, parent: fee_kinds(:baden_wuerttemberg_kind))
+  end
 
   before do
+    # Fabricate(Group::Stamm::Stammesmitgliederverwaltung.sti_name, person:, group:)
+    Fabricate(Group::Stamm::Stammesschatzmeister.sti_name, person:, group:)
+
     sign_in(person)
-    Fabricate(Group::Bundesgeschaeftsstelle::Bundesgeschaeftsfuehrung.sti_name, person:, group:)
   end
 
   it "GET#index does list fee kinds" do
@@ -25,17 +31,19 @@ describe FeeKindsController do
   it "POST#create creates a new fee kind" do
     expect do
       post :create, params: {
-        group_id: Group.root.id,
+        group_id: group.id,
         fee_kind: {
           name: "Test Foo",
-          layer_id: groups(:root),
-          role_type: "Cool::Role::Type",
-          restricted: true
+          layer_id: group.layer_group.id,
+          parent_id: fee_kinds(:baden_wuerttemberg_kind)
         }
       }
+
+      expect(assigns(:fee_kind)).to be_valid
+      expect(flash[:notice]).to eq(
+        "Beitragsart <i>Test Foo</i> wurde erfolgreich erstellt."
+      )
     end.to change { FeeKind.count }.by 1
-    expect(flash[:notice]).to eq "Beitragsart <i>Test Foo</i> wurde " \
-                                 "erfolgreich erstellt."
   end
 
   it "PUT#update updates fee kind" do
