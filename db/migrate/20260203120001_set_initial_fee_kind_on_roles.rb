463733f20f69961fdb31992d5994c54904710a18
Matthias Viehweger <kronn@kronn.de>
2026-02-17 16:08:52 +0100

    Exisiting Roles may need a FeeKind as well
    So let's assign one, either by migration of with a rake-task

---
+++ db/migrate/20260203120001_set_initial_fee_kind_on_roles.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2026-2026, BdP and DPSG. This file is part of
+#  hitobito_pfadi_de and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_pfadi_de.
+
+class SetInitialFeeKindOnRoles < ActiveRecord::Migration[8.0]
+  def up
+    types = Role.all_types.select(&:has_fee_kind)
+    scope = Role.where(type: types, fee_kind_id: nil)
+
+    say_with_time("Setting the initial FeeKind on Role where needed") do
+      say("Handling #{scope.count} Roles", :subitem)
+
+      scope.in_batches do |batch|
+        batch.each do |role|
+          role.update(fee_kind: FeeKindChooser.new(role).default)
+        end
+
+        $stderr.print "."
+      end
+      $stderr.puts
+    end
+  end
+
+  def down
+    say_with_time("Removing the initial FeeKind on Role") do
+      Role.update_all(fee_kind_id: nil)
+    end
+  end
+end
---
+++ lib/tasks/fee_kinds.rake
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2026-2026, BdP and DPSG. This file is part of
+#  hitobito_pfadi_de and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_pfadi_de.
+
+namespace :fee_kinds do
+  desc "Set initial FeeKind on Role where needed"
+  task set_initial: [:environment] do
+    scope = Role.where(type: types, fee_kind_id: nil)
+
+    warn "Trying to set initial FeeKind on #{scope.count} Roles..."
+
+    scope.find_each do |role|
+      result = role.update(fee_kind: FeeKindChooser.new(role).default)
+      $stderr.print result ? "." : "F"
+    end
+  end
+end
