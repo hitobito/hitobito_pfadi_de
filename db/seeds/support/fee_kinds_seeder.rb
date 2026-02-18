d401d879fc3f42971108733f41fc38f96da3bffe
carlobeltrame <carlo@beltra.me>
2026-02-18 18:04:08 +0100

    Seed some fee rates

--- db/seeds/support/fee_kinds_seeder.rb
+++ db/seeds/support/fee_kinds_seeder.rb
@@ -11,6 +11,21 @@ class FeeKindsSeeder
     "Sonderumlage", "Projektbeitrag", "Aktivitätsgebühr"
   ]
 
+  FEE_RATE_NAMES = [
+    "Normal", "Reduziert", "Spezial", "Jugend", "Neumitglied"
+  ]
+
+  FEE_RATE_VALIDITY_DATES = [
+    2.years.ago.beginning_of_year,
+    1.year.ago.beginning_of_year,
+    6.months.ago.beginning_of_month,
+    1.day.ago,
+    0.days.ago,
+    6.months.from_now.beginning_of_month,
+    1.year.from_now.beginning_of_year,
+    2.years.from_now.beginning_of_year,
+  ]
+
   def seed_fee_kinds
     Role.all_types.select(&:has_fee_kind).each do |role_type|
       root_fee_kind = FeeKind.seed_once(
@@ -29,6 +44,30 @@ class FeeKindsSeeder
           FeeKind.seed_once(name: FEE_KIND_NAMES.sample, parent: landesverband_fee_kind, layer: stamm)
         end
       end
+    end
+
+    FeeKind.find_each do |fee_kind|
+      3.times do
+        valid_from, valid_until = rand(2) > 0 ? [FEE_RATE_VALIDITY_DATES.sample, nil] : FEE_RATE_VALIDITY_DATES.sample(2).minmax
+        FeeRate.seed(:fee_kind_id, :amount, :valid_from, :valid_until, {
+          fee_kind_id: fee_kind.id,
+          name: FEE_RATE_NAMES.sample,
+          amount: rand(50),
+          valid_from:,
+          valid_until:,
+          max_member_months: [nil, nil, 6, 12].sample,
+          max_age: [nil, nil, 10, 18].sample
+        })
+      end
+      FeeRate.seed(:fee_kind_id, :amount, :valid_from, :valid_until, {
+        fee_kind_id: fee_kind.id,
+        name: FEE_RATE_NAMES.sample,
+        amount: rand(50),
+        valid_from: FEE_RATE_VALIDITY_DATES.select(&:past?).sample,
+        valid_until: nil,
+        max_member_months: [nil, nil, 6, 12].sample,
+        max_age: [nil, nil, 10, 18].sample
+      })
     end
   end
 end
